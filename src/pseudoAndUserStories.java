bea173833fa0745bb41d9e9c90facae35732ca3a
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2024-09-25 13:12:05 +0200

    planering

--- src/pseudoAndUserStories.java
+++ src/pseudoAndUserStories.java
@@ -1,43 +1,47 @@
 /*
+Planering {
+    - pseudo
+    - userStories
+    - skapa klasser för att börja planera ut vad varje ska göra och hur dem ska connecta
+    - börja tänka på VG kraven
+    -
+
+}
 MåsteKlasserAttHaIProgrammet {
-     User
-     Transaction
-        Expense - ärver frånTrans
-        Income - ärver från Trans
-     ExpenseStorage
-     IncomeStorage
-     EnumExpenseCategory
-     EnumIncomeCategory
-     BudgetTracker//Main
+    - User
+    - Transaction
+       - Expense - ärver frånTrans
+       - Income - ärver från Trans
+    - ExpenseStorage
+    - IncomeStorage
+    - EnumExpenseCategory
+    - EnumIncomeCategory
+    - BudgetTracker//Main
 }
 ExtraKlasser ??? {
-Meny
+    - Meny
 
 }
 UserStories ?? {
-    Möjlighet att lägg till custom "Enum" kategorier?
-
-
-
+    - Möjlighet att lägg till custom "Enum" kategorier?
 
 }
 VG ? {
+    - Du ska kunna förklara och redogöra för det objektorienterade principer du
+    implementerat i din app. (Redogör i din analys, se sista punkten).
 
-- Du ska kunna förklara och redogöra för det objektorienterade principer du
-implementerat i din app. (Redogör i din analys, se sista punkten).
+    - Du ska ha implementerat polymorfism i din app samt förklara hur du uppnåt
+    polymorfism. (Visa tydligt i din kod samt redogör i din analys, se sista
+    punkten).
 
-- Du ska ha implementerat polymorfism i din app samt förklara hur du uppnåt
-polymorfism. (Visa tydligt i din kod samt redogör i din analys, se sista
-punkten).
+    - Man ska kunna spara sina inkomster och utgifter per månad och även se sina
+    inkomster och utgifter per månad.
 
-- Man ska kunna spara sina inkomster och utgifter per månad och även se sina
-inkomster och utgifter per månad.
+    - Man ska kunna söka på en specifik utgift eller inkomst.
 
-- Man ska kunna söka på en specifik utgift eller inkomst.
-
-- Du ska även på ett fördjupat och självständigt sätt kunna förklara hur du tänkt,
-planerat och byggt din app. Det här gör du genom att bifoga en skriftlig
-sammanfattning och analys på max två A4 sidor.
+    - Du ska även på ett fördjupat och självständigt sätt kunna förklara hur du tänkt,
+    planerat och byggt din app. Det här gör du genom att bifoga en skriftlig
+    sammanfattning och analys på max två A4 sidor.
 
 }
 
