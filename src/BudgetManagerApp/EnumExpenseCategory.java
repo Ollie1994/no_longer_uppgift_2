9690d69e98889070604b9ff9504e232dd26d810f
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2024-09-25 13:45:50 +0200

    adding Enum categories

--- src/BudgetManagerApp/EnumExpenseCategory.java
+++ src/BudgetManagerApp/EnumExpenseCategory.java
@@ -1,4 +1,12 @@
 package BudgetManagerApp;
 
 public class EnumExpenseCategory {
+    public EnumExpenseCategory() {} // Empty constructor
+    enum Housing {
+        MORTGAGE, RENT, PROPERTYTAXES, HOUSEHOLDREPAIRS, HOAFEES, OTHER
+    }
+    enum Transportation {
+        CARPAYMENT, CARWARRANTY, GAS, MAINTENANCE, PARKINGFEES, REPAIRS, REGRISTATIONANDDMVFEES, OTHER
+    }
+    enum
 }
--- src/BudgetManagerApp/EnumIncomeCategory.java
+++ src/BudgetManagerApp/EnumIncomeCategory.java
@@ -1,4 +1,13 @@
 package BudgetManagerApp;
 
 public class EnumIncomeCategory {
+    public EnumIncomeCategory() {} // Empty constructor
+    enum IncomeCategories {
+        SALARY, STUDYGRANT, STUDENTLOANINCOME, GOVERMENTGRANTS, GIFT, BACKTAX,
+
+    }
 }
+
+
+
+    //Konstructor
