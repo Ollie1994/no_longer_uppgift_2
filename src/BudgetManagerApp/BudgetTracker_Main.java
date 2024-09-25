a9a87e41f4bb46128bafdca271b8bb50cab4cfca
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2024-09-25 14:43:00 +0200

    testat lite i main med User

--- src/BudgetManagerApp/BudgetTracker_Main.java
+++ src/BudgetManagerApp/BudgetTracker_Main.java
@@ -2,7 +2,11 @@ package BudgetManagerApp;
 
 public class BudgetTracker_Main {
     public static void main(String[] args) {
-
+        // testar lite under bara
+        User user1 = new User("Oliwer", "Karlsson");
+        System.out.println(user1); // här måste vi omvandla från klass USER till klass string för att skriva ut
+        System.out.println(user1.getFirstName() + " " + user1.getLastName()); // här behöver vi itne det pga getter
+        user1.displayUser(); // samam här, pga av metod sp blir det utskivit
 
     }
 }
--- src/BudgetManagerApp/User.java
+++ src/BudgetManagerApp/User.java
@@ -1,4 +1,22 @@
 package BudgetManagerApp;
 
 public class User {
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    // ----------------------- METHODS -----------------------------------------------------
+    public void displayUser () {
+        System.out.println("User: " + getFirstName() + " " + getLastName());
+    }
+
+    // ----------------------- G & S --------------------------------------------------------
+    public String getFirstName() {return firstName;}
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+    public String getLastName() {return lastName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
 }
+
